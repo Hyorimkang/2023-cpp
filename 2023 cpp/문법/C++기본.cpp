8a76f0035dc2ed5f2335dd98617be156980856ac
Hyorimkang <s2219@e-mirim.hs.kr>
2023-03-17 09:45:03 +0900

    개행객체 namespace

--- 2023 cpp/문법/C++기본.cpp
+++ 2023 cpp/문법/C++기본.cpp
@@ -1,9 +1,13 @@
 ﻿//cout 객체 사용
 #include <iostream>
+
+//이름공간 (중복이름 방지)
+using namespace std;
+
 int main() {
 	// << 출력연산자
 	// endl : 개행
-	std::cout << "Hello World" << std::endl;
+	cout << "Hello World" << endl;
 
 	return 0;
 }
