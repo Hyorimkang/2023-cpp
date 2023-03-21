9a1a9b6b615ea4d794ae89055baae9c48919c418
Hyorimkang <s2219@e-mirim.hs.kr>
2023-03-21 13:56:46 +0900

    참조(reference)

--- 2023 cpp/문법/참조.cpp
+++ 2023 cpp/문법/참조.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
 	int a = 30;  //a : 30
-	int* pa = &a;  //pa : a의 주소값
+	int& ra = a;  //ra : a의 주소값
 
-	*pa = 100;  //pa가 가리키고 있는 대상 : 100 : a
+	ra = 100;  //ra가 가리키고 있는 대상 : 100 : a
 
 	cout << "a의 값은 " << a << endl;
 
