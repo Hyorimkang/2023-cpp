f28a6b800f9035342e3ee4fa14c86f9115865758
HyorimKang <s2219@e-mirim.hs.kr>
2023-04-18 14:41:53 +0900

    멤버변수 동적할당

--- 2023 cpp/문법/클래스 중급.cpp
+++ 2023 cpp/문법/클래스 중급.cpp
@@ -1,22 +1,25 @@
 ﻿#include <iostream>
+#include <String.h>
 
 using namespace std;
 
 class MString {
 public :
 	MString(const char* str) {
-		size_ = sizeof(str);
-		c_str_ = str;
+		unsigned int l = strlen(str);
+		c_str_ = new char[l+1];  //'\0'(널문자)가 들어갈 공간 +1
+		strcpy(c_str_, str);
+		size_ = l;
 	}
 	unsigned int size(void) {
 		return size_;
 	}
-	const char* c_str(void) {
+	char* c_str(void) {
 		return c_str_; //_(언더 스코어) 멤버변수라는 뜻
 	}
 private:
 	unsigned int size_; //문자열의 길이
-	const char* c_str_; //문자열의 시작주소
+	char* c_str_; //문자열의 시작주소
 };
 
 int main() {
