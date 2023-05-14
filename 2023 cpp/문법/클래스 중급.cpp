e8f7c7e4f4641297156d525ab686ae6fdf6330c8
HyorimKang <s2219@e-mirim.hs.kr>
2023-05-14 20:07:51 +0900

    정적할당으로 변경

--- 2023 cpp/문법/클래스 중급.cpp
+++ 2023 cpp/문법/클래스 중급.cpp
@@ -1,46 +1,37 @@
 ﻿#include <iostream>
-#include <String.h>
+#include <string.h>
 
 using namespace std;
 
 class MString {
-public :
-	MString(const char* str) {
+public:
+	MString(const char* str)
+	{
 		unsigned int l = strlen(str);
-		c_str_ = new char[l+1];  //'\0'(널문자)가 들어갈 공간 +1
+		c_str_ = new char[l + 1];		// '\0'(널문자)가 들어갈 공간 +1
 		strcpy(c_str_, str);
 		size_ = l;
 		cout << "MString 생성자 호출 완료" << endl;
 	}
 
-	//소멸자 (destructor)
-	~MString() {
-		//소멸자로 생성자에서 동적할당한 메모리 해제
-		//소멸자의 매개변수는 없다.
-		delete[]  c_str_;
+	// 소멸자(destructor)
+	~MString()
+	{
+		// 소멸자로 생성자에서 동적할당한 메모리 해제
+		delete[] c_str_;
 		cout << "MString 소멸자 호출 완료" << endl;
 	}
-
-	unsigned int size(void) {
-		return size_;
-	}
-	char* c_str(void) {
-		return c_str_; //_(언더 스코어) 멤버변수라는 뜻
-	}
-
+	unsigned int size(void) { return size_; }
+	char* c_str(void) { return c_str_; }
 private:
-	unsigned int size_; //문자열의 길이
-	char* c_str_; //문자열의 시작주소
+	unsigned int size_;		// 문자열의 길이
+	char* c_str_;			// 문자열의 시작주소
 };
 
-int main() {
-	//생성자가 호출되면서 "I will be back"(문자열)만큼의 동적할당이 이뤄짐
-	MString* str = new MString("I will be back");
-	cout << str->c_str() << endl;
-
-	//객체 str만 삭제, 동적할당으로 생성된 문자열은 그대로 남아있음 (메모리 누수)
-	//TODO : 소멸자 (destuctor)를 이용해 동적할당된 메모리도 해제하기
-	delete str;
+int main(void)
+{
+	MString str = MString("I will be back");
+	cout << str.c_str() << endl;
 
 	return 0;
 }
