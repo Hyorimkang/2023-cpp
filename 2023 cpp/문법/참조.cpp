d0872edff3e82bd5eff66a245d39e203d6f9b618
HyorimKang <s2219@e-mirim.hs.kr>
2023-05-16 13:52:31 +0900

    일반적인 생성자, 소멸자

--- 2023 cpp/문법/참조.cpp
+++ 2023 cpp/문법/참조.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-int main() {
-	int a = 30;  //a : 30
-	int& ra = a;  //ra : a의 주소값
+int main(void)
+{
+	int a = 30;
+	int& ra = a;
 
-	ra = 100;  //ra가 가리키고 있는 대상 : 100 : a
-
+	ra = 100;			// ra가 가리키고 있는 대상(a) : 100
 	cout << "a의 값은 " << a << endl;
 
 	return 0;
