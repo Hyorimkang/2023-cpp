7ed657a1b39c147ad77f0c2698f8296ac9cd789a
HyorimKang <s2219@e-mirim.hs.kr>
2023-05-16 14:26:29 +0900

    복사생성자(얕은 복사)

--- 2023 cpp/문법/복사생성자.cpp
+++ 2023 cpp/문법/복사생성자.cpp
@@ -1,16 +1,24 @@
 ﻿#include <iostream>
 #include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
 
 
 using namespace std;
 
 class Munja {
 public:
+	//일반생성자
 	Munja(const char* qz) {
 		// 3("abc") + 1('\0')
 		str_ = new char[strlen(qz) + 1];
 		strcpy(str_, qz);
 		cout << "일반생성자 호출" << endl;
+	}
+
+	//복사생성자 (얕은 복사)
+	Munja(const Munja& hj): str_(hj.str_)
+	{
+		cout << "복사생성자 호출" << endl;
 	}
 
 	~Munja() {
@@ -23,6 +31,8 @@ private:
 };
 
 int main() {
-	Munja m1 = Munja("abc");
+	Munja m1 = Munja("abc"); //일반생성자 호출
+	Munja m2 = m1; //복사생성자 호출
+
 	return 0;
 }
