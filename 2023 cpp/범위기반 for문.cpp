87be53b6c8d760ad39f5ea847718c5a6be0ffc51
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-10-17 14:35:59 +0900

    auto를 통하여 자료형을 자동으로 맞춰준다

--- 2023 cpp/범위기반 for문.cpp
+++ 2023 cpp/범위기반 for문.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
-int main() {
+int main(void) {
 	double a[3] = { 5.6, 6.7, 7.8 };
-	for (double x : a) {
+	
+	//누가봐도 a의 자료형은 int이다
+	auto b = 4;
+
+	//x의 자료형은 double로 자동설정
+	for (auto x : a) {
 		cout << a << endl;
 	}
 	return 0;
