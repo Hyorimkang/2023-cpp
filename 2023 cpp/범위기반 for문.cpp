d5ac8dc3e08d439af67466207ec7de7d4d3cf039
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-10-17 14:31:26 +0900

    범위기반 for문

--- 2023 cpp/범위기반 for문.cpp
+++ 2023 cpp/범위기반 for문.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-	int a[3] = { 5,6,7 };
-	for (int i = 0; i < 3; i++) {
-		cout << a[i] << endl;
+	double a[3] = { 5.6, 6.7, 7.8 };
+	for (double x : a) {
+		cout << a << endl;
 	}
 	return 0;
 }
